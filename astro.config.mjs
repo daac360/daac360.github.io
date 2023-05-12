e8367c75fef686267995f8b9c4e6b50f4f918b1f
daac360 <daac360@outlook.com>
2023-05-13 01:25:59 +0200

    Grumpy time 1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://daac360.github.io',
-    base: '/',
+  site: 'https://daac360.github.io',
+  base: '/',
+  integrations: [react(), tailwind()]
 });
